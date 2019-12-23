8ebb1ba2a781647afcc5a61dfc1de595a130bbbb
Josip Bošnjak <jbosnjak@unipu.hr>
2019-12-23 19:44:47 +0100

    added aditional class
    added aditional class which will be used to parse json file with jakson as their attributes now we only need to integrate this whole class into the rest of the application

--- src/com/cmd/concept/LastSourceAndDestination.java
+++ src/com/cmd/concept/LastSourceAndDestination.java
@@ -1,26 +1,55 @@
 package com.cmd.concept;
 
 import java.io.File;
-
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class LastSourceAndDestination {
 	 public static void main(String[] args) {
-		 
-		 //here app will ask user to open external file
-		 System.out.println("Open last saved source and destination directory? 1 for yes 0 for no");
-		 //boolean input if user opens then file will be parsed else make json objects from chosen directory
-		 
-		 String sourceDirectory="asourceDirectory";
+		 String sourceDirectory="sourceDirectory";
 		 String destinationDirectory="destinationDirectory";
+		 boolean open=false;
+		 Scanner scanner=new Scanner(System.in);
+		 //here app will ask user to open external file
+		 System.out.println("Open last saved source and destination directory? true for yes false for no");
+		 //boolean input if user opens then file will be parsed else make json objects from chosen directory
+		 open=scanner.nextBoolean();
+		 scanner.nextLine();
+		 if(open==true) {
+			 System.out.println("Choose file which contains last saved source and destination directory:");
+			 String lastFile=scanner.nextLine();
+			 System.out.println("Reading json source files and parsing it to the variable...");
+			 ObjectMapper map=new ObjectMapper();
+			 try {
+				SourceDestination sd=map.readValue(new File(lastFile), SourceDestination.class);
+				System.out.println(sd.getSourceDirectory()+"\n"+sd.getDestinationDirectory());
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 System.exit(0);
+		 }else {
+			 System.out.println("Enter source and destination directory, etc...");
+			 System.exit(0);
+		 }
+		
 		 
 		 //save last saved source and destination directory to json file located from user choice
 		 //before loading, user must include json file location
@@ -32,9 +61,9 @@ public class LastSourceAndDestination {
 		 //the string source and destination directory
 		 //add to the map source and destination directory keys are source directory and destination directory
 		 //values input from keyboard from user 
-		 Map<String, String> mapa=new HashMap<String, String>();
-		 mapa.put("sourceDirectory", sourceDirectory);
-		 mapa.put("destinationDirectory", destinationDirectory);
+		// Map<String, String> mapa=new HashMap<String, String>();
+		 //mapa.put("sourceDirectory", sourceDirectory);
+		 //mapa.put("destinationDirectory", destinationDirectory);
 		 
 		 //user will choose output directory
 		 //directories.json are output files which will contain values from map
@@ -56,7 +85,7 @@ public class LastSourceAndDestination {
 			generator.useDefaultPrettyPrinter();
 			mapper.writeTree(generator, rootNode);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		 
---
+++ src/com/cmd/concept/SourceDestination.java
@@ -0,0 +1,34 @@
+package com.cmd.concept;
+//will be used by jackson to parse json file 
+public class SourceDestination {
+	String sourceDirectory;
+	String destinationDirectory;
+	
+	public SourceDestination() {
+		
+	}
+	public SourceDestination(String sourceDirectory, String destinationDirectory) {
+		this.sourceDirectory = sourceDirectory;
+		this.destinationDirectory = destinationDirectory;
+	}
+	public String getSourceDirectory() {
+		return sourceDirectory;
+	}
+	public void setSourceDirectory(String sourceDirectory) {
+		this.sourceDirectory = sourceDirectory;
+	}
+	public String getDestinationDirectory() {
+		return destinationDirectory;
+	}
+	public void setDestinationDirectory(String destinationDirectory) {
+		this.destinationDirectory = destinationDirectory;
+	}
+	@Override
+	public String toString() {
+		return "SourceDestination [sourceDirectory=" + sourceDirectory + ", destinationDirectory="
+				+ destinationDirectory + "]";
+	}
+	
+	
+
+}
