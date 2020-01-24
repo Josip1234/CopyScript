84698eac32949febe67e78f552dba6484df7e874
Josip Bošnjak <jbosnjak@unipu.hr>
2020-01-24 18:21:42 +0100

    added swing designer

---
+++ src/com/cmd/swing/CopyScript.java
@@ -0,0 +1,43 @@
+package com.cmd.swing;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class CopyScript {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CopyScript window = new CopyScript();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CopyScript() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
