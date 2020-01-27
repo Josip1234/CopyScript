2f9b445ea4f31c35421b3bbd7dc06f765f33adcd
Josip Bošnjak <jbosnjak@unipu.hr>
2020-01-27 19:52:45 +0100

    added choose source directory action button and file chooser

--- src/com/cmd/swing/CopyScript.java
+++ src/com/cmd/swing/CopyScript.java
@@ -5,16 +5,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
 import java.awt.GridBagConstraints;
 import javax.swing.SwingConstants;
+
+import javafx.stage.FileChooser;
+
 import java.awt.Insets;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.SystemColor;
+import javax.swing.JPanel;
 
 public class CopyScript {
 
 	private JFrame frame;
-	private JTextField txtChosenSource;
-	private JTextField txtChosenDestination;
 
 	/**
 	 * Launch the application.
@@ -45,69 +55,34 @@ public class CopyScript {
 	private void initialize() {
 		String title="Copy script Application";
 		frame = new JFrame(title);
+		frame.getContentPane().setBackground(SystemColor.window);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 		
-		txtChosenSource = new JTextField();
-		txtChosenSource.setText("Chosen source...");
-		GridBagConstraints gbc_txtChosenSource = new GridBagConstraints();
-		gbc_txtChosenSource.insets = new Insets(0, 0, 5, 5);
-		gbc_txtChosenSource.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtChosenSource.gridx = 2;
-		gbc_txtChosenSource.gridy = 0;
-		frame.getContentPane().add(txtChosenSource, gbc_txtChosenSource);
-		txtChosenSource.setColumns(10);
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.text);
+		frame.getContentPane().add(panel);
 		
-		JButton btnChooseSource = new JButton("Choose source");
-		btnChooseSource.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_btnChooseSource = new GridBagConstraints();
-		gbc_btnChooseSource.insets = new Insets(0, 0, 5, 0);
-		gbc_btnChooseSource.gridx = 8;
-		gbc_btnChooseSource.gridy = 0;
-		frame.getContentPane().add(btnChooseSource, gbc_btnChooseSource);
-		
-		txtChosenDestination = new JTextField();
-		txtChosenDestination.setText("Chosen destination...");
-		GridBagConstraints gbc_txtChosenDestination = new GridBagConstraints();
-		gbc_txtChosenDestination.insets = new Insets(0, 0, 5, 5);
-		gbc_txtChosenDestination.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtChosenDestination.gridx = 2;
-		gbc_txtChosenDestination.gridy = 1;
-		frame.getContentPane().add(txtChosenDestination, gbc_txtChosenDestination);
-		txtChosenDestination.setColumns(10);
-		
-		JButton btnChooseDestination = new JButton("Choose destination");
-		GridBagConstraints gbc_btnChooseDestination = new GridBagConstraints();
-		gbc_btnChooseDestination.insets = new Insets(0, 0, 5, 0);
-		gbc_btnChooseDestination.gridx = 8;
-		gbc_btnChooseDestination.gridy = 1;
-		frame.getContentPane().add(btnChooseDestination, gbc_btnChooseDestination);
-		
-		JButton btnMoveFile = new JButton("Move file");
-		GridBagConstraints gbc_btnMoveFile = new GridBagConstraints();
-		gbc_btnMoveFile.insets = new Insets(0, 0, 5, 5);
-		gbc_btnMoveFile.gridx = 2;
-		gbc_btnMoveFile.gridy = 2;
-		frame.getContentPane().add(btnMoveFile, gbc_btnMoveFile);
-		
-		JButton btnListOfSource = new JButton("List of source");
-		GridBagConstraints gbc_btnListOfSource = new GridBagConstraints();
-		gbc_btnListOfSource.insets = new Insets(0, 0, 5, 0);
-		gbc_btnListOfSource.gridx = 8;
-		gbc_btnListOfSource.gridy = 2;
-		frame.getContentPane().add(btnListOfSource, gbc_btnListOfSource);
-		
-		JButton btnListOfDestination = new JButton("List of destination");
-		GridBagConstraints gbc_btnListOfDestination = new GridBagConstraints();
-		gbc_btnListOfDestination.gridx = 8;
-		gbc_btnListOfDestination.gridy = 3;
-		frame.getContentPane().add(btnListOfDestination, gbc_btnListOfDestination);
+		JButton btnChooseSourceDirectory = new JButton("Choose source directory");
+		btnChooseSourceDirectory.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser=new JFileChooser();
+				fileChooser.setBackground(SystemColor.window);
+				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				fileChooser.setDialogTitle("Choose source directory:");
+				fileChooser.setApproveButtonText("Open directory:");
+				int returnValue=fileChooser.showOpenDialog(null);
+				if(returnValue==JFileChooser.APPROVE_OPTION) {
+					File seleFile = fileChooser.getSelectedFile();
+					System.out.println("You selected: "+ seleFile);
+				}
+				
+			}
+		});
+		panel.add(btnChooseSourceDirectory);
 		
 	}
 
