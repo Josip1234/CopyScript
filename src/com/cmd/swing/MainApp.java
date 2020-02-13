0abfbad819c51982409dc0d83b2ecfdd75f09795
Josip Bošnjak <jbosnjak@unipu.hr>
2020-02-13 19:31:33 +0100

    update

--- src/com/cmd/swing/MainApp.java
+++ src/com/cmd/swing/MainApp.java
@@ -9,6 +9,11 @@ import java.awt.Window.Type;
 import javax.swing.JMenuItem;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.Action;
 import java.awt.Dialog.ModalExclusionType;
 
@@ -82,6 +87,7 @@ public class MainApp {
 		
 		JMenuItem mntmOpenLastDirectories = new JMenuItem("Open last directories");
 		mnDirectoryOperations.add(mntmOpenLastDirectories);
+	
 		
 		JMenuItem mntmOpenDirectories = new JMenuItem("Open source directory");
 		mnDirectoryOperations.add(mntmOpenDirectories);
@@ -103,7 +109,37 @@ public class MainApp {
 		JMenu mnTerminateProgram = new JMenu("Terminate program");
 		menuBar.add(mnTerminateProgram);
 		
-	}
+	mntmOpenLastDirectories.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			   System.out.println(e.getActionCommand());
+			   System.out.println(mntmCopyFile.getActionCommand());
+			   
+				
+			}
+		});
+		
+		
+		}
+};
+/*		
+mntmOpenLastDirectories.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mntmCopyFile.setEnabled(true);
+				mntmMoveFile.setEnabled(true);
+				mntmCreateNewFile.setEnabled(true);
+			    mntmChooseFile.setEnabled(true);
+			    mntmDeleteFile.setEnabled(true);
+			    mntmListFilesIn.setEnabled(true);
+			    mntmListFilesIn_1.setEnabled(true);
+			    
+				
+			}
+		});
+		
+	}*/
 
-	
-}
+
