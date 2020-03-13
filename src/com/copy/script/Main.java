b3cd848a7b626ace0a1f241549f95900fc3255a9
Josip Bošnjak <jbosnjak@unipu.hr>
2020-03-13 21:01:15 +0100

    update

--- src/com/copy/script/Main.java
+++ src/com/copy/script/Main.java
@@ -1,14 +1,16 @@
 package com.copy.script;
 
-import java.util.Scanner;
 
+
+import com.copy.script.console.input.KeyboardInput;
 import com.copy.script.messages.Message;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+		KeyboardInput input=new KeyboardInput();
+		input.setIntPointer();
 		
 		String operations=Message.folderOperations+"\n"+Message.listOperations+"\n";
 		String directoryOperations=Message.chooseDirectoryLocation+"\n"+Message.chooseDirectoryForFileCopy;
@@ -19,33 +21,16 @@ public class Main {
 		System.out.println(operations);
 	
 		int key=0;
-		/***
-		 * 
-		 * "resource leak" implies that some system resource (usually memory) is being lost or wasted needlessly. 
-		 * Usually this will impact you when you start getting OutOfMemoryErrors 
-		 * thrown during the normal operation of your program.
-		 * As others have said, you need to call 'close' on IO classes.
-		 * This ensures that your Scanner is always closed, guaranteeing proper resource cleanup
-		 * 
-		 * 
-		 *
-		try {
-			key = scanner.nextInt();
-		} finally {
-			scanner.close();
-		}*/
-		key = scanner.nextInt();
+	
+		key = input.integerInput();
 		switch (key) {
 		case 1:
 			System.out.println(Message.mainFolderOperations);
 			System.out.println(Message.chooseSubmenuItem);
 			System.out.println(directoryOperations);
 			
-			try {
-				key = scanner.nextInt();
-			} finally {
-				scanner.close();
-			}
+			key = input.integerInput();
+			
 			switch (key) {
 			case 1:
 				System.out.println(Message.chosenMapFolder);
@@ -67,11 +52,8 @@ public class Main {
 			System.out.println(Message.listOfFilesInFolder);
 			System.out.println(Message.chooseAList);
 			System.out.println(listForSelection+"\n"+executeFiles);
-			try {
-				key = scanner.nextInt();
-			} finally {
-				scanner.close();
-			}
+			
+			key = input.integerInput();
 			switch (key) {
 			case 1:
 				System.out.println(Message.choosenFiles);
