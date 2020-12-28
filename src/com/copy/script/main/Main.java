325531f8733f5c70497c97acc25f68e09fc5044c
Josip Bošnjak <jbosnjak@unipu.hr>
2020-12-28 13:46:43 +0100

    added main class

---
+++ src/com/copy/script/main/Main.java
@@ -0,0 +1,16 @@
+package com.copy.script.main;
+/**
+ * 
+ * @author Korisnik
+ * @category MainCMDApp
+ * @version 1.0
+ * @since 28.12.2020. 13:46
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
