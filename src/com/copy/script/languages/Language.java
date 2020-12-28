8d9c331b323c924bd8881cd07fbca382b3a25621
Josip Bošnjak <jbosnjak@unipu.hr>
2020-12-28 13:58:33 +0100

    added enum class for language menu

---
+++ src/com/copy/script/languages/Language.java
@@ -0,0 +1,12 @@
+package com.copy.script.languages;
+
+/**
+ * 
+ * @author Korisnik
+ * @category Languages
+ * @version 1.0
+ * @since 28.12.2020. 13:57
+ */
+public enum Language {
+   HR,EN,DE;
+}
